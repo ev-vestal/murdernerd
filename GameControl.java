d1eef1daf72ffb3564433a66ac50659abf850f84
jfunchess <37041982+jfunchess@users.noreply.github.com>
2019-11-11 18:25:29 -0600

    Update GameControl.java
    Combined me and Xinrui-Li's gameControl Class

--- GameControl.java
+++ GameControl.java
@@ -1,13 +1,68 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ComponentAdapter;
+
 public class GameControl {
+    
+    private gameStats getEntity;
+    private VoteManger voteNow;
+    private JButton startGameButton;
+    private JPanel panel1;
+    private JPanel panel2;
+    private JButton quitGameButton;
+    private JTextField textField1;
 
     public static void main(String[] args)
     {
+        new Player().setPassClue();
+        new gameStats();
+        
+        JFrame frame=new JFrame("Murdernerd");
+        frame.setLocation(500,500);
+        frame.setSize(500,500);
+        frame.setContentPane(new GameControler().panel1);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
 
     }
+    
+    public GameControler()
+    {
+        startGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JWindow w=new JWindow();
+                JOptionPane.showMessageDialog(null,"Connecting to setting...");
+                JFrame frame2=new JFrame("Find out who is Murderer");
+                frame2.setLocation(400,400);
+                frame2.setSize(500,500);
+                frame2.setBackground(Color.CYAN);
+                frame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                frame2.pack();
+                frame2.setVisible(true);
 
-    private Player playerTurn;
-    private gameStat getEntity;
-    private voteManager voteNow;
+            }
+        });
+
+
+        quitGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                    System.exit(0);
+
+            }
+        });
+        startGameButton.addComponentListener(new ComponentAdapter() {
+
+        });
+
+
     //private turnManager
 
 }
