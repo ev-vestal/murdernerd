0b24ca76f7e748770acc2cbdd4088404be87d7e3
Xinrui-Li <57157136+Xinrui-Li@users.noreply.github.com>
2019-11-23 11:01:04 -0600

    This is my GameControler by Xinrui

--- GameControler.java
+++ GameControler.java
@@ -5,6 +5,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
+import java.io.File;
 
 public class GameControler {
 
@@ -12,15 +13,14 @@ public class GameControler {
     private VoteManger voteNow;
     private JButton startGameButton;
     private JPanel panel1;
-    private JPanel panel2;
     private JButton quitGameButton;
     private JTextField textField1;
-
     public static void main(String[] args)
     {
         JFrame frame=new JFrame("Murdernerd");
         frame.setLocation(500,500);
         frame.setSize(500,500);
+        frame.setBackground(Color.ORANGE);
         frame.setContentPane(new GameControler().panel1);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.pack();
@@ -28,20 +28,81 @@ public class GameControler {
     }
     public GameControler()
     {
+        //VoteManger vote=new VoteManger();
+        //Entity player=new Entity();
+        //ImageIcon image=new ImageIcon("background.jpg");
+        panel1.setBackground(Color.PINK);
         startGameButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 JWindow w=new JWindow();
-                JOptionPane.showMessageDialog(null,"Connecting to setting...");
+                JOptionPane.showMessageDialog(null,"Ready? Press OK to start the game.");
                 JFrame frame2=new JFrame("Find out who is Murderer");
                 frame2.setLocation(400,400);
                 frame2.setSize(500,500);
+                frame2.setBounds(200,200,200,200);
                 frame2.setBackground(Color.CYAN);
+                JButton frameTwoVoteButton=new JButton("Vote Now");
+                frame2.add(frameTwoVoteButton,BorderLayout.SOUTH);
+                frameTwoVoteButton.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        JWindow w2=new JWindow();
+                        JFrame frame3=new JFrame("Time to Vote!");
+                        frame3.setLocation(600,600);
+                        frame3.setSize(500,500);
+                        frame3.setBackground(Color.magenta);
+                        JButton firstNPCButton=new JButton();
+                        firstNPCButton.setText("NPC 1");
+                        JButton secondNPCButton=new JButton();
+                        secondNPCButton.setText("NPC 2");
+                        JButton thirdNPCButton=new JButton();
+                        thirdNPCButton.setText("NPC 3");
+                        frame3.add(firstNPCButton,BorderLayout.WEST);
+                        frame3.add(secondNPCButton,BorderLayout.CENTER);
+                        frame3.add(thirdNPCButton,BorderLayout.EAST);
+
+                        firstNPCButton.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                JWindow w3=new JWindow();
+                                if(e.getActionCommand().equals("NPC 1"))
+                                {
+                                    JOptionPane.showMessageDialog(null,"YOU LOSE!");
+                                }
+                            }
+                        });
+                        secondNPCButton.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                JWindow w3=new JWindow();
+                                if(e.getActionCommand().equals("NPC 2"))
+                                {
+                                    JOptionPane.showMessageDialog(null,"YOU LOSE!");
+                                }
+                            }
+                        });
+                        thirdNPCButton.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                JWindow w3=new JWindow();
+                                if(e.getActionCommand().equals("NPC 3"))
+                                {
+                                    JOptionPane.showMessageDialog(null,"YOU WIN!");
+                                }
+                            }
+                        });
+                        frame3.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                        frame3.pack();
+                        frame3.setVisible(true);
+
+                    }
+                });
                 frame2.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
                 frame2.pack();
                 frame2.setVisible(true);
-
             }
+
         });
 
 
@@ -52,11 +113,8 @@ public class GameControler {
 
             }
         });
-        startGameButton.addComponentListener(new ComponentAdapter() {
 
-        });
     }
-
 
 
 }
